eb942f4570bba24b0a7825326c2b73f7352701ed
Bartek-KA <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-01-17 23:23:35 +0100

    PacjentRepository

---
+++ Repositories/PacjentRepo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrzychodniaFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrzychodniaFinal.Repos
+{
+    public class PacjentRepo : IPacjentRepo
+    {
+        private readonly PrzychodniaContext _dbContext;
+        public PacjentRepo(PrzychodniaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddNewPatient(Pacjenci pacjent)
+        {
+            _dbContext.Add(pacjent);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeletePatient(Pacjenci pacjent)
+        {
+            _dbContext.Remove(pacjent);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<Pacjenci>> GetAllPatients()
+        {
+            return await _dbContext.Pacjencis.ToListAsync();
+        }
+
+        public async Task<Pacjenci> GetPatientById(int id)
+        {
+            return await _dbContext.Pacjencis.FindAsync(id);
+        }
+
+        public async Task UpdatePatientDetails(Pacjenci pacjent)
+        {
+            _dbContext.Entry(pacjent).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
