429d9f0e0fea10b602fb193457432ffd2df100a4
Bartek-KA <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-01-17 23:54:43 +0100

    PacjentController

---
+++ Controllers/PacjentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrzychodniaFinal.Models;
+using PrzychodniaFinal.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrzychodniaFinal.Controllers
+{
+    public class PacjentController : Controller
+    {
+        private readonly IPacjentRepo _pacjentRepo;
+        public PacjentController(IPacjentRepo pacjentRepo)
+        {
+            _pacjentRepo = pacjentRepo;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Pacjenci pacjenci)
+        {
+            if (ModelState.IsValid)
+            {
+                await _pacjentRepo.AddNewPatient(pacjenci);
+                return RedirectToAction("Index");
+            }
+            return View(pacjenci);
+        }
+    }
+}
