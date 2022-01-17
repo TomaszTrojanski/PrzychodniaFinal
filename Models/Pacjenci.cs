72db600946a6f0998665dc2bc388739be5bb08f5
Bartek-KA <bartlomiej.kazior@microsoft.wsei.edu.pl>
2022-01-17 22:33:54 +0100

    ModelPacjenci

--- Models/Pacjenci.cs
+++ Models/Pacjenci.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,17 +8,32 @@ namespace PrzychodniaFinal.Models
 {
     public partial class Pacjenci
     {
+        [Key]
+
+        public int IdPacjenta { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Imię Pacjenta")]
+        [Display(Name = "Imię")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Nazwisko Pacjenta")]
+        [Display(Name = "Nazwisko")]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Numer Pesel Pacjenta")]
+        [Display(Name = "Pesel")]
+        [StringLength(11)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Wprowadź cyfry")]
+        public string Pesel { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Adres Pacjenta")]
+        [Display(Name = "Adres")]
+        public string AdresZamieszkania { get; set; }
+
+        public virtual ICollection<Choroby> Chorobies { get; set; }
         public Pacjenci()
         {
             Chorobies = new HashSet<Choroby>();
         }
-
-        public int IdPacjenta { get; set; }
-        public string Pesel { get; set; }
-        public string Imie { get; set; }
-        public string Nazwisko { get; set; }
-        public string AdresZamieszkania { get; set; }
-
-        public virtual ICollection<Choroby> Chorobies { get; set; }
     }
 }
