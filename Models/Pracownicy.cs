ee1039418ec3d5a182938251a2f14251c236226a
Jakub Kliś <jakubklis2201@gmail.com>
2022-01-17 22:52:11 +0100

    ModelPracownicy

--- Models/Pracownicy.cs
+++ Models/Pracownicy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,13 +8,41 @@ namespace PrzychodniaFinal.Models
 {
     public partial class Pracownicy
     {
+        [Key]
+
         public int IdPracownika { get; set; }
-        public string Pesel { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Id Pracownika")]
+        [Display(Name = "Id")]
         public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Numer Pesel Pracownika")]
+        [Display(Name = "Pesel")]
+        [StringLength(11)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Wprowadź cyfry")]
+        public string Pesel { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Imię Pracownika")]
+        [Display(Name = "Imię")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Nazwisko Pracownika")]
+        [Display(Name = "Nazwisko")]
         public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Adres Pracownika")]
+        [Display(Name = "AdresZamieszkania")]
         public string AdresZamieszkania { get; set; }
-        public DateTime DataZatrudnienia { get; set; }
-        public DateTime KoniecKontraktu { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Datę Zatrudnienia Pracownika")]
+        [Display(Name = "DataZatrudnienia")]
+        [RegularExpression("d/M/yyyy", "dd/M/yyyy", "d/MM/yyyy", "dd/MM/yyyy"), ErrorMessage = "Wprowadź datę")]
+        public string DataZatrudnienia { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź Koniec Kontraktu Pracownika")]
+        [Display(Name = "KoniecKontraktu")]
+        [RegularExpression("d/M/yyyy", "dd/M/yyyy", "d/MM/yyyy", "dd/MM/yyyy", ErrorMessage = "Wprowadź datę")]
+        public string KoniecKontraktu { get; set; }
 
         public virtual Lekarze Lekarze { get; set; }
     }
