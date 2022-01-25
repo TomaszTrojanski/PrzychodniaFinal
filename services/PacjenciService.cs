0fa1564d38f2e3d1328c3a691839f652b08f216d
TomaszTrojanski <79982724+TomaszTrojanski@users.noreply.github.com>
2022-01-25 22:07:12 +0100

    zmiana architektury

---
+++ services/PacjenciService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PrzychodniaFinal.DataAccess;
+
+namespace PrzychodniaFinal.services
+{
+    public class PacjenciService
+    {
+        private readonly PrzychodniaDBContext _context;
+        public PacjenciService(PrzychodniaDBContext context)
+        {
+            _context = context;
+        }
+        public void Edit()
+        {
+            if (id != pacjenci.PacjenciID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pacjenci);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PacjenciExists(pacjenci.PacjenciID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pacjenci);
+        }
+    }
+}
